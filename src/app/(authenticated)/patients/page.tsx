32a62e98903e20b21f94afe47eb840f998daa01e
Kgaogel09 <kgaogelothorn@gmail.com>
2025-10-26 15:58:50 +0200

    feat: create Patients component for authenticated users

---
+++ src/app/(authenticated)/patients/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Patients() {
+
+    return (
+
+        <main className="container mx-auto p-4 mt-16 h-screen">
+            <h1 className="text-4xl">Welcome to your Patients!</h1>
+        </main>
+
+    )
+}
+
+export default Patients
