141a704587625cf2407e25ce565774714371924a
Kgaogel09 <kgaogelothorn@gmail.com>
2025-10-26 15:58:12 +0200

    feat: add layout component for authenticated users

---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Header } from "@/components/layout/header";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    );
+}
