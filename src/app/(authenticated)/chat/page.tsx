96816cdce5adcb7a3241d57d154352c1b196d41f
Kgaogel09 <kgaogelothorn@gmail.com>
2025-10-26 15:58:23 +0200

    feat: create Chat component for authenticated users

---
+++ src/app/(authenticated)/chat/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+function Chat() {
+
+    return (
+
+        <main className="container mx-auto p-4 mt-16 h-screen">
+            <h1 className="text-4xl">Welcome to your Chat Bot!</h1>
+        </main>
+
+    )
+}
+
+export default Chat
