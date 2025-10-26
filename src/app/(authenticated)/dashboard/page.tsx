d16f50a3dfbc02c3608c0ccce0107923bf9c89f5
Kgaogel09 <kgaogelothorn@gmail.com>
2025-10-26 15:58:34 +0200

    feat: add Dashboard component for authenticated users

---
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Dashboard() {
+    return (
+        <main className="container mx-auto p-4 mt-16 h-screen">
+            <h1 className="text-4xl">Welcome to your Dashboard!</h1>
+        </main>
+    );
+}
