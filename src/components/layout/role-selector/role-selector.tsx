d9a4c426bb51a3da8bbeadc6cb12d5d9682a30eb
Kgaogel09 <kgaogelothorn@gmail.com>
2025-10-28 11:13:38 +0200

    feat: add role selector component
    Implement role selection functionality for user interface

---
+++ src/components/layout/role-selector/role-selector.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Toggle } from "@/components/ui/toggle";
+import { useAuth } from "@/context/auth";
+import { UserRole } from "@/types/types";
+import { Stethoscope, User } from "lucide-react";
+import { useState } from "react";
+
+export function RoleSelector() {
+    const { userProfile, updateUserRole } = useAuth();
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    const handleRoleToggle = async (pressed: boolean) => {
+        if (!userProfile) return;
+
+        const newRole: UserRole = pressed ? 'clinician' : 'patient';
+
+        setIsUpdating(true);
+        try {
+            await updateUserRole(newRole);
+        } catch (error) {
+            console.error("Failed to update role:", error);
+        } finally {
+            setIsUpdating(false);
+        }
+    };
+
+    if (!userProfile) return null;
+
+    const isClinician = userProfile.role === 'clinician';
+
+    return (
+
+        <Toggle
+            pressed={isClinician}
+            disabled={isUpdating}
+            onPressedChange={handleRoleToggle}
+            aria-label="Toggle bookmark"
+            size="sm"
+            variant="outline"
+            className="data-[state=on]:bg-transparent  rounded-full bg-blue-50 border border-blue-200 ml-4 px-3 py-1"
+        >
+            {isClinician ? <Stethoscope className="h-3 w-3 text-blue-600" /> : <User className="h-3 w-3 text-blue-600" />}
+            <span className="text-sm font-medium text-blue-700 capitalize">{isClinician ? 'Clinician' : 'Patient'}</span>
+
+        </Toggle>
+    );
+}
